9a24dc4aef2d8a312ae1b8ba81622c38d43a4932
micheldavrdz <micheldavrdz@gmail.com>
2022-12-26 20:32:18 -0600

    fix: apikey env typo

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ import { Configuration, OpenAIApi } from "openai";
 dotenv.config();
 
 const config = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey: process.env.OPEN_API_KEY,
 });
 
 const openai = new OpenAIApi(config);
